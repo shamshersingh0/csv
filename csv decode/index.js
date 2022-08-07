15c9db9e596bbdd27c3532bb9391ad787f3540a3
shamshersingh0 <95402470+shamshersingh0@users.noreply.github.com>
2022-08-07 18:28:02 +0530

    csv files

--- csv decode/index.js
+++ csv decode/index.js
@@ -1,14 +1,40 @@
 const csvtojson = require('csvtojson')
 const fs = require('fs')
+const multer = require('multer')
+const express = require('express')
+const app = express()
 
-const csvfilepath = "Book1.csv"
+const fileStoreEngine = multer.diskStorage({
+    destination:(req, res, cb) => {
+        cb(null, "./csv")
+    },
+    filename:(req, file, cb) => {
+        cb(null, Date.now()+"--"+ file.originalname)
+    }
+})
 
-csvtojson()
-.fromFile(csvfilepath)
-.then((json) => {
-    console.log(json)
 
-    fs.writeFileSync('output.json', JSON.stringify(json),"utf-8", (err) => {
-       if(err) console.log(err)
-    })
+const upload = multer({storage: fileStoreEngine})
+
+
+
+app.post("/single",upload.single("csvs"),(req, res)=> {
+    console.log(req.file)
+    res.send("SINGLE FILE UPLOAD SUCCESS")
+})
+
+
+// const csvfilepath = "Book1.csv"
+// csvtojson()
+// .fromFile(csvfilepath)
+// .then((json) => {
+//     console.log(json)
+
+//     fs.writeFileSync('output.json', JSON.stringify(json),"utf-8", (err) => {
+//        if(err) console.log(err)
+//     })
+// })
+
+app.listen(3000, () =>{
+    console.log("running")
 })
